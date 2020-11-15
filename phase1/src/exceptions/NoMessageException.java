1c621952f4645e0e505b394e7de4bc0858ad62c2
jon23 <jon23lam@gmail.com>
2020-11-15 16:52:11 -0500

    Adding exception Javadoc

--- phase1/src/exceptions/NoMessageException.java
+++ phase1/src/exceptions/NoMessageException.java
@@ -1,5 +1,9 @@
 package exceptions;
 
+
+/**
+ * Exception class for when there is no message to be found.
+ */
 public class NoMessageException extends Exception{
     public NoMessageException(String errorMessage){
         super(errorMessage);
