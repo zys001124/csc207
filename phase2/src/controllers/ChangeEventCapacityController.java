844418193db9e7b88e86a65bee60726e908f72d5
Yaosheng Zhang <yaosheng.zhang@mail.utoronto.ca>
2020-12-04 14:35:36 +0800

    Fixes

--- phase2/src/controllers/ChangeEventCapacityController.java
+++ phase2/src/controllers/ChangeEventCapacityController.java
@@ -26,15 +26,6 @@ public class ChangeEventCapacityController extends Controller {
     @FXML // fx:id="eventListField"
     private ListView<Label> eventListField; // Value injected by FXMLLoader
 
-    @FXML // fx:id="timeField"
-    private TextField timeField; // Value injected by FXMLLoader
-
-    @FXML // fx:id="eventNameField"
-    private TextField eventNameField; // Value injected by FXMLLoader
-
-    @FXML // fx:id="roomNameField"
-    private TextField roomNameField; // Value injected by FXMLLoader
-
     @FXML // fx:id="capacityField"
     private TextField capacityField; // Value injected by FXMLLoader
 
@@ -56,26 +47,20 @@ public class ChangeEventCapacityController extends Controller {
     void onChangeButtonClicked(ActionEvent event) {
 
         String label = "";
-
-        String time = timeField.getText();
-        String room = roomNameField.getText();
+        String inter = eventListField.getSelectionModel().getSelectedItem().getText().split("             Room")[0];
+        String eventname = inter.split(": ")[1];
         String capacity = capacityField.getText();
-        String eventName = eventNameField.getText();
 
-        InputProcessResult result = changeEventCapacity(time, room, capacity, eventName);
+        InputProcessResult result = changeEventCapacity(capacity, eventname);
 
         if (result == InputProcessResult.SUCCESS) {
             label = "Capacity change successful.";
         } else if (result == InputProcessResult.EVENT_DOES_NOT_EXIST) {
             label = "This event does not exist. Try again.";
-        } else if (result == InputProcessResult.INVALID_TIME){
-            label = "Time is in the wrong format. Try again.";
-        } else if (result == InputProcessResult.INVALID_INPUT){
-            label = "Invalid input. Try again.";
-        } else if (result == InputProcessResult.WRONG_ROOM){
-            label = "This room is wrong. Try again.";
         } else if (result == InputProcessResult.CAPACITY_OVER) {
-            label = "This event is over room capacity. Try again.";
+            label = "This is over room capacity. Try again.";
+        } else if (result == InputProcessResult.INVALID_INPUT) {
+            label = "Invalid input. Try again.";
         }
         createMessageLabel.setText(label);
     }
@@ -84,25 +69,17 @@ public class ChangeEventCapacityController extends Controller {
     @FXML
         // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
-        assert timeField != null : "fx:id=\"timeField\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
         assert capacityField != null : "fx:id=\"capacityField\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
-        assert roomNameField != null : "fx:id=\"roomNameField\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
-        assert eventNameField != null : "fx:id=\"eventNameField\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
         assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
         assert changeButton != null : "fx:id=\"changeButton\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
         assert createMessageLabel != null : "fx:id=\"createMessageLabel\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
         assert eventListField != null : "fx:id=\"eventListField\" was not injected: check your FXML file 'Change Event Capacity.fxml'.";
     }
 
-    private InputProcessResult changeEventCapacity(String time, String r, String c, String event) {
-        if(time.equals("") || r.equals("") || c.equals("") || event.equals("")){
+    private InputProcessResult changeEventCapacity(String c, String event) {
+        if(checkIntFormat(c)){
             return InputProcessResult.INVALID_INPUT;
         }
-
-        if(checkIntFormat(r,c)){
-            return InputProcessResult.INVALID_INPUT;
-        }
-        int room = Integer.parseInt(r);
         int capacity = Integer.parseInt(c);
         if (capacity > 60){
             return InputProcessResult.CAPACITY_OVER;
@@ -111,46 +88,13 @@ public class ChangeEventCapacityController extends Controller {
             return InputProcessResult.EVENT_DOES_NOT_EXIST;
         }
         Event currentEvent = eventManager.getEvent(event);
-        if(!checkDateFormat(time)){
-            return InputProcessResult.INVALID_TIME;
-        }
-        LocalDateTime sDateTime = getLocalDateTime(time);
-        if(room<0 || room>5){
-            return InputProcessResult.WRONG_ROOM;
-        }
-
-        if(currentEvent.getEventRoom() == room && !currentEvent.getEventTime().equals(sDateTime)){
-            return InputProcessResult.INVALID_TIME;
-        }
-        if(currentEvent.getEventRoom() != room && currentEvent.getEventTime().equals(sDateTime)){
-            return InputProcessResult.WRONG_ROOM;
-        }
-        if(currentEvent.getEventRoom() != room && !currentEvent.getEventTime().equals(sDateTime)){
-            return InputProcessResult.INVALID_INPUT;
-        }
 
         currentEvent.setEventCapacity(capacity);
         return InputProcessResult.SUCCESS;
     }
 
-    private LocalDateTime getLocalDateTime(String time) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        return LocalDateTime.parse(time,formatter);
-    }
-
-    private boolean checkDateFormat(String time){
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
-        try {
-            formatter.parse(time.trim());
-        } catch (DateTimeParseException pe) {
-            return false;
-        }
-        return true;
-    }
-
-    private boolean checkIntFormat(String room, String capacity){
+    private boolean checkIntFormat(String capacity){
         try{
-            Integer.parseInt(room);
             Integer.parseInt(capacity);
         }catch (NumberFormatException e) {
             return true;
